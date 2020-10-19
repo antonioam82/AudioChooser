fb12c8cce3dce45f6dbfa71ac2440ef85108f275
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-10-19 19:34:38 +0200

    Update audio_chooser.py

--- audio_chooser.py
+++ audio_chooser.py
@@ -6,15 +6,28 @@ import time
 import os
 from VALID import OKI
 
+def async_playback(filename):
+    data, fs = sf.read(filename)
+    sd.play(data,fs)
+    return data, fs
+
 def select_audio():
-    for elem,tema in enumerate(lista_temas):
-        print(elem,tema)
-    eleccion = OKI(input("Introduzca número correspondiente a su eleccion: "))
-    while int(eleccion) > (len(lista_temas)-1):
-        eleccion = OKI(input("Introduzca indice válido correspondiente a su opción: "))
-    assert eleccion in range(len(lista_temas))
-    audio_selec = lista_temas[eleccion]
-    print("AUDIO SELECCIONADO: {}".format(audio_selec))
+    while True:
+        for elem,tema in enumerate(lista_temas):
+            print(elem,tema)
+        eleccion = OKI(input("Introduzca número correspondiente a su eleccion: "))
+        while int(eleccion) > (len(lista_temas)-1):
+            eleccion = OKI(input("Introduzca indice válido correspondiente a su opción: "))
+        assert eleccion in range(len(lista_temas))
+        audio_selec = lista_temas[eleccion]
+        print("AUDIO SELECCIONADO: {}".format(audio_selec))
+        async_playback(audio_selec)
+        op = input("Opcion: ")
+        if op == "STOP":
+            sd.stop()
+        elif op == "END":
+            sd.stop()
+            break
 
 
 direc = input("Introduce directorio: ")
