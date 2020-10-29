b09798a9faf255c1771f2d56ca32878ddaaa3eae
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-10-29 12:05:47 +0100

    Update audio_chooser.py

--- audio_chooser.py
+++ audio_chooser.py
@@ -8,9 +8,7 @@ import threading
 import pyttsx3
 from VALID import OKI
 
-
 nums = {'cero':0,'uno':1,'dos':2,'tres':3,'cuatro':4,'cinco':5,'seis':6,'siete':7,'ocho':8,'nueve':9}
-#C:\Users\Antonio\Documents\videos\audios
 
 def async_playback(filename):
     data, fs = sf.read(filename)
