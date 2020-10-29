4eb1f5acb2d9f698088cd37f583f2eca10824cce
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-10-29 17:21:01 +0100

    Update audio_chooser.py

--- audio_chooser.py
+++ audio_chooser.py
@@ -10,7 +10,6 @@ from VALID import OKI
 
 list_inn = False
 nums = {'cero':0,'uno':1,'dos':2,'tres':3,'cuatro':4,'cinco':5,'seis':6,'siete':7,'ocho':8,'nueve':9}
-#C:\Users\Antonio\Documents\videos\audios
 
 def async_playback(filename):
     global list_inn
