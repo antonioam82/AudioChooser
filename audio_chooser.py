6b26e9f750afd12613486e8e025f75160e501c9f
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-10-31 23:26:09 +0100

    Update audio_chooser.py

--- audio_chooser.py
+++ audio_chooser.py
@@ -2,11 +2,9 @@ import glob
 import speech_recognition as sr
 import sounddevice as sd
 import soundfile as sf
-#import time
 import os
 import threading
 import pyttsx3
-from VALID import OKI
 
 list_inn = False
 nums = {'cero':0,'uno':1,'dos':2,'tres':3,'cuatro':4,'cinco':5,'seis':6,'siete':7,'ocho':8,'nueve':9}
