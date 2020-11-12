cb0ed7856d43de6fb2c9305b826fcf83d1f8fa50
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-11-12 19:25:22 +0100

    Update audio_chooser2.py

--- audio_chooser2.py
+++ audio_chooser2.py
@@ -11,6 +11,7 @@ direc = pickle.load(open('directorios','rb'))
 list_inn = False
 nums = {'cero':0,'uno':1,'dos':2,'tres':3,'cuatro':4,'cinco':5,'seis':6,'siete':7,'ocho':8,'nueve':9,
         'diez':10,'once':11,'doce':12,'trece':13,'catorce':14,'quince':15}
+#C:\Users\Antonio\Documents\videos\audios
 
 def async_playback(filename):
     global list_inn
@@ -105,6 +106,8 @@ def select_audio():
             break
         elif op == 'cambia micrófono':
             cambia_microfono()
+        elif op == 'comandos':
+            comandos()
     
 def speaker(content,v):
     engine.say(content)
